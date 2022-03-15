730c4183fe37dcc0039ff2ed0b6e2e218fff38ef
zhumingpeng <mingpeng.zhu@xbongbong.com>
2022-03-15 11:21:46 +0800

    feat: 倒数组件

---
+++ src/components/CountDown/CountDown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <slot :isCountingDown="isCountingDown" :num="num"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountDown',
+
+  props: {
+    initNum: {
+      type: Number,
+      default: 5
+    }
+  },
+
+  data () {
+    return {
+      isCountingDown: false,
+      num: this.initNum
+    }
+  },
+
+  methods: {
+    startCountDown () {
+      this.num = this.initNum
+      this.isCountingDown = true
+      this.countDownHandler()
+    },
+    countDownHandler () {
+      window.setTimeout(() => {
+        this.num = this.num - 1
+
+        if (this.num === 0) {
+          this.isCountingDown = false
+          return
+        }
+
+        this.countDownHandler()
+      }, 1000)
+    }
+  }
+}
+</script>
